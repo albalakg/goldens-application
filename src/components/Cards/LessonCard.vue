f6bf168d09afabd082c7de5a39949da0433172e2
gal <gala@hackeruso.com>
2022-04-16 11:14:49 +0300

    feat(lesson card)

--- src/components/Cards/LessonCard.vue
+++ src/components/Cards/LessonCard.vue
@@ -1,12 +1,22 @@
 
 <template>
-  <div class="lesson_card_wrapper">
-    <img :src="lesson.image" alt="lesson image" loading="lazy">
+  <div class="lesson_card_wrapper rounded">
+    <img class="rounded" :src="lesson.image" alt="lesson image" loading="lazy">
+    <div class="lesson_card_darkner"></div>
+    <div class="lesson_card_details">
+      <p>{{ lesson.name }}</p>
+      <divider />
+      <small>
+        {{lesson.description}}
+      </small>
+    </div>
   </div>
 </template>
 
 <script>
+import Divider from '../General/Divider.vue';
 export default {
+  components: { Divider },
 
   props: {
     lesson: {
@@ -35,6 +45,23 @@ export default {
       position: absolute;
       width: 100%;
       height: 100%;
+      left: 0;
+      top: 0;
+    }
+
+    .lesson_card_details {
+      z-index: 3;
+      position: absolute;
+      bottom: 5%;
+      text-align: center;
+    }
+
+    .lesson_card_darkner {
+      z-index: 2;
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      background: linear-gradient(#0000, #0008);
     }
   }
 
