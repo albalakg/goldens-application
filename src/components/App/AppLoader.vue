a69c573497294611d23ab9c476fafcb6c7f6ef21
Gal Albalak <gala@hackeruso.com>
2022-07-01 20:29:07 +0300

    feat(application loader)

--- src/components/App/AppLoader.vue
+++ src/components/App/AppLoader.vue
@@ -1,13 +1,18 @@
 <template>
 <div class="app_loader_wrapper">
-    <h1>
-        טוען...
-    </h1>
+    <v-progress-circular size="150" indeterminate class="pa-4" color="#d5b26e">
+        <favicon />
+    </v-progress-circular>
 </div>
 </template>
 
 <script>
+import Favicon from '../General/Favicon.vue'
 export default {
+
+    components: {
+        Favicon,
+    },
 
 }
 </script>
@@ -22,10 +27,6 @@ export default {
         align-items: center;
         background-color: #31353d;
         z-index: 1000;
-
-        h1 {
-            color: #fff;
-        }
     }
 
 </style>
