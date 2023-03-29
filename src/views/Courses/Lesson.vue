74df6293a4476200c5a928897b98667470277aca
Blacky <gal.blacky@gmail.com>
2023-03-29 17:37:08 +0300

    fix(lesson start time)

--- src/views/Courses/Lesson.vue
+++ src/views/Courses/Lesson.vue
@@ -299,7 +299,6 @@ export default {
 
     setVideoStartTime() {
       const isPassedMostOfVideo = (this.startTime * 100 / this.lesson.video.video_length) > 80;
-      console.log(this.startTime, this.lesson.progress);
       if(this.$refs.video && !isPassedMostOfVideo) {
         this.$refs.video.setStartTime(this.startTime)
       }
