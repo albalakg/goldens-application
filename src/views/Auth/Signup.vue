a8070e3ec4eed415adbcdfa7ffee5959315aec30
Blacky <gal.blacky@gmail.com>
2023-05-25 14:26:01 +0300

    feat(add scrollToView in signup)

--- src/views/Auth/Signup.vue
+++ src/views/Auth/Signup.vue
@@ -2,93 +2,193 @@
     <div class="signin_wrapper auth_padding_top">
         <main class="auth_form_top_margin w100">
             <v-flex d-flex flex-wrap>
-                <v-flex xs12 md6 lg7 xl8>
+                <v-flex xs12 md6 lg6>
                     <star-logo colored class="star_image" />
                 </v-flex>
-                <v-flex xs12 md6 lg5 xl4 px-5 px-md-0 mb-md-8>
-                    <v-flex md8>
+                <v-flex xs12 md6 lg6 px-5 px-md-0 mb-md-8>
+                    <v-flex md8 lg10 xl9>
                         <v-form class="signin_form" ref="form" @submit.prevent="submit()">
                             <h2 class="auth_form_title"><span class="main_text_color">הרשמה</span> לאתר</h2>
                             <h3 class="auth_form_subtitle">ברוך הבא לעולם הספורט</h3>
                             
                             <Divider :space="8" />
-                            
-                            <email-input
-                                ref="email"
-                                outlined
-                                title
-                                icon
-                                @onChange="setEmail" 
-                            >
-                            </email-input>
+
+                            <v-flex v-if="$vuetify.breakpoint.mdAndUp">
+                                <v-flex d-flex>
+                                    <v-flex xs6 class="ml-1">
+                                        <email-input
+                                            ref="email"
+                                            outlined
+                                            title
+                                            icon
+                                            @onChange="setEmail" 
+                                        >
+                                        </email-input>
+                                    </v-flex>
+                                    <v-flex xs6 class="mr-1">
+                                        <password-input
+                                            ref="password"
+                                            outlined
+                                            title
+                                            icon
+                                            autocomplete="new-password"
+                                            @onChange="setPassword" 
+                                        >
+                                        </password-input>
+                                    </v-flex>
+                                </v-flex>
+                                
+                                <div class="spacer"></div>
+                                <div class="spacer"></div>
+
+                                <v-flex d-flex>
+                                    <v-flex xs6 class="ml-1">
+                                        <first-name-input
+                                            ref="firstName"
+                                            outlined
+                                            title
+                                            icon
+                                            @onChange="setFirstName" 
+                                        >
+                                        </first-name-input>
+                                    </v-flex>
+                                    <v-flex xs6 class="mr-1">
+                                        <last-name-input
+                                            ref="lastName"
+                                            outlined
+                                            title
+                                            icon
+                                            @onChange="setLastName" 
+                                        >
+                                        </last-name-input>
+                                    </v-flex>
+                                </v-flex>
+                                
+                                <div class="spacer"></div>
+                                <div class="spacer"></div>
+
+                                <v-flex d-flex>
+                                    <v-flex xs6 class="ml-1">
+                                        <team-input 
+                                            ref="team"
+                                            outlined
+                                            title
+                                            optional
+                                            icon
+                                            @onChange="setTeam" 
+                                        />
+                                    </v-flex>
+                                    <v-flex xs6 class="mr-1">
+                                        <city-input 
+                                            ref="city"
+                                            outlined
+                                            title
+                                            optional
+                                            icon
+                                            @onChange="setCity" 
+                                        />
+                                    </v-flex>
+                                </v-flex>
+                                
+                                <div class="spacer"></div>
+                                <div class="spacer"></div>
+                                
+                                <v-flex d-flex>
+                                    <v-flex xs6 class="ml-1">
+                                        <birthdate-input 
+                                            ref="birthdate"
+                                            outlined
+                                            title
+                                            optional
+                                            icon
+                                            @onChange="setBirthdate" 
+                                        />
+                                    </v-flex>
+                                    <v-flex xs6 class="mr-1">
+                                    </v-flex>
+                                </v-flex>
+                            </v-flex>
+
+                            <v-flex v-else>
+                                    
+                                <email-input
+                                    ref="email"
+                                    outlined
+                                    title
+                                    icon
+                                    @onChange="setEmail" 
+                                >
+                                </email-input>
+                                
+                                <div class="spacer"></div>
+
+                                <password-input
+                                    ref="password"
+                                    outlined
+                                    title
+                                    icon
+                                    autocomplete="new-password"
+                                    @onChange="setPassword" 
+                                >
+                                </password-input>
+                                
+                                <div class="spacer"></div>
+
+                                <first-name-input
+                                    ref="firstName"
+                                    outlined
+                                    title
+                                    icon
+                                    @onChange="setFirstName" 
+                                >
+                                </first-name-input>
+
+                                <div class="spacer"></div>
+
+                                <last-name-input
+                                    ref="lastName"
+                                    outlined
+                                    title
+                                    icon
+                                    @onChange="setLastName" 
+                                >
+                                </last-name-input>
+                                
+                                <div class="spacer"></div>
+                                
+                                <team-input 
+                                    ref="team"
+                                    outlined
+                                    title
+                                    optional
+                                    icon
+                                    @onChange="setTeam" 
+                                />
+
+                                <div class="spacer"></div>
+
+                                <city-input 
+                                    ref="city"
+                                    outlined
+                                    title
+                                    optional
+                                    icon
+                                    @onChange="setCity" 
+                                />
+
+                                <div class="spacer"></div>
+
+                                <birthdate-input 
+                                    ref="birthdate"
+                                    outlined
+                                    title
+                                    optional
+                                    icon
+                                    @onChange="setBirthdate" 
+                                />
+                            </v-flex>
                             
                             <div class="spacer"></div>
-
-                            <password-input
-                                ref="password"
-                                outlined
-                                title
-                                icon
-                                autocomplete="new-password"
-                                @onChange="setPassword" 
-                            >
-                            </password-input>
-                            
-                            <div class="spacer"></div>
-
-                            <first-name-input
-                                ref="firstName"
-                                outlined
-                                title
-                                icon
-                                @onChange="setFirstName" 
-                            >
-                            </first-name-input>
-
-                            <div class="spacer"></div>
-
-                            <last-name-input
-                                ref="lastName"
-                                outlined
-                                title
-                                icon
-                                @onChange="setLastName" 
-                            >
-                            </last-name-input>
-                            
-                            <div class="spacer"></div>
-                            
-                            <team-input 
-                                ref="team"
-                                outlined
-                                title
-                                optional
-                                icon
-                                @onChange="setTeam" 
-                            />
-
-                            <div class="spacer"></div>
-
-                            <city-input 
-                                ref="city"
-                                outlined
-                                title
-                                optional
-                                icon
-                                @onChange="setCity" 
-                            />
-
-                            <div class="spacer"></div>
-
-                            <birthdate-input 
-                                ref="birthdate"
-                                outlined
-                                title
-                                optional
-                                icon
-                                @onChange="setBirthdate" 
-                            />
-                            
                             <div class="spacer"></div>
 
                             <div>
@@ -216,15 +316,27 @@ export default {
         },
 
         validate() {
-            const isEmailValid      = this.$refs.email.validate();
-            const isPasswordValid   = this.$refs.password.validate();
-            const isFirstNameValid  = this.$refs.firstName.validate();
-            const isLastNameValid   = this.$refs.lastName.validate();
-            const isTeamValid       = this.$refs.team.validate();
-            const isCityValid       = this.$refs.city.validate();
-            const isBirthdateValid   = this.$refs.birthdate.validate();
+            const fields = [
+                'email', 'password', 'firstName', 'lastName', 'team', 'city', 'birthdate'
+            ];
 
-            return isEmailValid && isPasswordValid && isFirstNameValid && isLastNameValid && isTeamValid && isCityValid && isBirthdateValid;
+            let isValid             = true;
+            let scrolledToInvalid   = false;
+
+            fields.forEach(field => {
+                const fieldElement = this.$refs[field];
+                const isFieldValid = fieldElement.validate();
+
+                if(!isFieldValid) {
+                    isValid = isFieldValid;
+                    if(!scrolledToInvalid) {
+                        fieldElement.$el.scrollIntoView({behavior: "smooth", block: "nearest", inline: "center"})
+                        scrolledToInvalid = true;
+                    }
+                }
+            })
+
+            return isValid;
         },
 
         setEmail(email) {
